802336bc63ba4fe212b30cedadf9d228abe83123
roxiefoxie <ulve_jente@hotmail.com>
2023-03-06 15:42:02 +0100

    Introduksjon-om-meg

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeColor(){
+    document.getElementById('app').style.backgroundColor = 'skyblue';
+}
+function changeBack(){
+    document.getElementById('app').style.backgroundColor = 'red';
+}
